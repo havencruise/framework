e3ce1e8b7d9feca3242cf3ecfd40becf323bd48e
Prashanth Padmanabh <prashanth128@gmail.com>
2014-05-07 13:01:16 -0400

    moved up everything

---
+++ bin/create_environment.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import sys, os, subprocess
+BASE_DIR = os.path.abspath(
+            os.path.join(os.path.dirname(os.path.abspath(__file__)), 
+            '../'))
+ENV_DIR = 'env'
+
+def create_env():
+    VENV = "".join([BASE_DIR, '/bin/virtualenv.py'])
+
+    #create virtual env
+    print 'Creating virtual environment in folder %s/%s' % (BASE_DIR, ENV_DIR)
+    subprocess.call([VENV, '%s/%s' % (BASE_DIR, ENV_DIR)])
+    print ''
+
+    #call pip and install base requirements
+    print 'Installing base requirements'
+    subprocess.call(['%s/%s/bin/pip' % (BASE_DIR, ENV_DIR),
+                     'install', '-r', '%s/requirements.txt' % (BASE_DIR)])
+    print ''
+
+
+
+def create_win_env():
+    VENV = "".join([BASE_DIR, '\\bin\\virtualenv.py'])
+
+    #create virtual env
+    print 'Creating virtual environment in folder %s\\%s' % (BASE_DIR, ENV_DIR)
+    p = subprocess.Popen([sys.executable, VENV, '%s\\%s' % (BASE_DIR, ENV_DIR)])
+    p.communicate()
+    print ''
+
+    #call pip and install base requirements
+    print 'Installing base requirements'
+    p = subprocess.Popen(['%s\\%s\\Scripts\\pip.exe' % (BASE_DIR, ENV_DIR),
+                          'install', '-r', '%s\\requirements.txt' % (BASE_DIR)])
+    p.communicate()
+    print ''
+
+
+if __name__ == '__main__':
+    if sys.platform != 'win32':
+        create_env()
+    else:
+        create_win_env()
+
+    print 'Base environments are created.'
+    print 'If you are checking into svn, make sure you don''t check in \
+            the env directory.  You can run `svn propset svn:ignore -F .svnignore .`'
+    if sys.platform == 'win32':
+        print 'Remember to source your environment via \
+                %s/Scripts/activate.bat before doing anything' % (ENV_DIR)
+    else:
+        print 'Remember to source your environment via \
+                %s/bin/activate before doing anything' % (ENV_DIR)
