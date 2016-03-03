99712b06c091e0996a4684b6751c94ae8a5b299d
havencruise <ppadmanabh@ppadmanabh.local>
2016-03-03 11:33:23 -0500

    remove unused ctx processor

--- conf/.framework/templates/settings/template.py
+++ conf/.framework/templates/settings/template.py
@@ -23,6 +23,4 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.core.context_processors.request",
     "django.core.context_processors.csrf",
-    "social.apps.django_app.context_processors.backends",
-    "social.apps.django_app.context_processors.login_redirect",
 )
